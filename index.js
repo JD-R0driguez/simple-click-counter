23a3e0897022739df1bfd70f6bc61e0b091f0d73
Juan <jd.rodriguez.or@gmail.com>
2023-04-13 16:52:07 -0400

    Define document structure

---
+++ index.js
@@ -0,0 +1,23 @@
+/* Create a counter program to increase and decrease a number
+
+1- Create the document elements for user input
+2- Create variables to read user input
+3- Modify html after user input
+4- Update the page style
+
+*/
+
+let counter = 0;
+
+function increase(){
+    counter += 1;
+}
+
+function decrease(){
+    counter -= 1;
+}
+
+function reset(){
+    counter = 0;
+}
+
